6315893f239979ec5d9c1a1a6e283abffdae6677
Henk-Jan Sleijster <henk-jan@sleijster.nl>
2025-02-16 19:34:32 +0100

    almost mvp

--- assets/pingpongeriser.js
+++ assets/pingpongeriser.js
@@ -1,16 +1,48 @@
 var moduleUrl = '/pingpongeriser/';
 
 $(function() {
+	if ($('.match').data('match')) {
+		getMatchData($('.match').data('match'));
+	}
+
 	$('.score-plus').on('click', function() {
-		$.post(moduleUrl + 'scorePlus', {side: $(this).data('side')}, function(data) {
+		var match = $('.match').data('match')
+		var side = $(this).parent().data('side');
+		$.post(moduleUrl + 'scorePlus', {match: match, side: side}, function(data) {
 			updateMatch(data);
 		});
 	});
+	$('.score-undo').on('click', function() {
+		var match = $('.match').data('match')
+		$.post(moduleUrl + 'scoreUndo', {match: match}, function(data) {
+			updateMatch(data);
+		});
+	});
+	$('.switch-sides').on('click', function() {
+		$('.side1').toggleClass('switched');
+	});
 });
 
+function getMatchData(match) {
+	$.get(moduleUrl + 'updateMatch', {match: match}, function(data) {
+		updateMatch(data);
+	});
+}
+
 function updateMatch(data) {
-	$('.side1 h1').text(data.side1.points);
-	$('.side1 h2').text(data.side1.games);
-	$('.side2 h1').text(data.side2.points);
-	$('.side2 h2').text(data.side2.games);
+	$('.match').data('match', data.match.id);
+	if (Object.keys(data.state).length) {
+		let state = data.state;
+
+		$('.side1 .player').text(state.side1.player);
+		$('.side1 .points').text(state.side1.points);
+		$('.side1 .games').text(state.side1.games);
+
+		$('.side2 .player').text(state.side2.player);
+		$('.side2 .points').text(state.side2.points);
+		$('.side2 .games').text(state.side2.games);
+	}
+
+	// $('.score-undo').toggle(state.side1.points + state.side1.games + state.side2.points + state.side2.games > 0);
+	$('.score-plus, .score-undo, .switch-sides').toggle(!data.match.finished_at);
 }
--- modules/Pingpongeriser.php
+++ modules/Pingpongeriser.php
@@ -1,15 +1,31 @@
 <?php
 
+use LiteriserDB as DB;
+
 class Pingpongeriser
 {
 	use LiteriserModule;
 
+	private $match_id;
+
 	public function init() {
-		$_SESSION['match'] = 1;
+		$this->match_id = $_GET['match'] ?? null;
+
+		$debug = 0;
+		if ($debug) {
+			header('Content-type: text/plain');
+			self::recalculateMatch($match);
+			$data = self::getMatchData($match);
+			echo '<pre>' . print_r($data, true) . '</pre>';
+			exit;
+		}
 	}
 
 	public function web() {
 		Literiser::setTitle(__CLASS__);
+		Literiser::addHeadTag('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>');
+		Literiser::addHeadTag('<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">');
+		Literiser::addHeadTag('<meta meta name="viewport" content="user-scalable=no">');
 		$r = '';
 
 		$r .= $this->renderMatch();
@@ -19,40 +35,125 @@ class Pingpongeriser
 
 	private function renderMatch() {
 		$r = '';
-		$r .= '<div class="match">';
-		$r .= $this->renderMatchSide(1);
-		$r .= $this->renderMatchSide(2);
+		$r .= '<div class="match" data-match="' . $this->match_id . '">';
+		$r .= $this->renderSide(1);
+		$r .= $this->renderSide(2);
+		$r .= '<span class="score-undo round-button">↶</span>';
+		$r .= '<span class="switch-sides round-button">⇄</span>';
 		$r .= '</div>';
 
 		return $r;
 	}
 
-	private function renderMatchSide($side) {
+	private function renderSide($side) {
 		$r = '';
 
-		$r .= '<div class="matchside side' . $side . '">';
-		$r .= '<h1>' . $side . '</h1>';
-		$r .= '<h2>0</h2>';
-		$r .= '<button data-side="' . $side . '" class="score-plus">+</button>';
+		$r .= '<div class="side side' . $side . '" data-side="' . $side . '">';
+		$r .= '<h1 class="player">&nbsp;</h1>';
+		$r .= '<span class="score-plus round-button">+</span>';
+		$r .= '<div class="points">&nbsp;</div>';
+		$r .= '<div class="games">&nbsp;</div>';
 		$r .= '</div>';
 
 		return $r;
 	}
 
+	private function getMatchData($match_id) {
+		$data = [
+			'match' => DB::row("
+				SELECT m.*, IFNULL(SUM(g.won_by_side = 1), 0) `side1_games`, IFNULL(SUM(g.won_by_side = 2), 0) `side2_games`
+				FROM matches m
+				LEFT JOIN games g ON m.id = g.match_id AND g.won_by_side IS NOT NULL
+				WHERE m.id = " . $match_id . "
+				GROUP BY m.id"
+			),
+			'games' => DB::rows(
+				"SELECT g.game, g.won_by_side, IFNULL(SUM(p.side = 1), 0) `side1_points`, IFNULL(SUM(p.side = 2), 0) `side2_points`
+				FROM games g
+				LEFT JOIN points p ON g.game = p.game AND p.match_id = g.match_id
+				WHERE g.match_id = " . $match_id . "
+				GROUP BY g.game
+				ORDER BY g.game"
+			),
+			'players' => DB::rows(
+				"SELECT mp.side, p.first_name, p.full_name
+				FROM match_players mp
+				INNER JOIN players p ON mp.player_id = p.id
+				WHERE mp.match_id = " . $match_id
+			),
+		];
 
-	private function xhr_scorePlus($args) {
-		// $r = [];
-
-		return $this->returnNumbers();
+		return $data;
 	}
 
-	private function returnNumbers() {
+	private function xhr_updateMatch($request) {
+		$data = self::getMatchData($request['match']);
+
 		$r = [];
-		$r['side1']['points'] = 10;
-		$r['side1']['games'] = 2;
-		$r['side2']['points'] = 8;
-		$r['side2']['games'] = 0;
+		$r['match'] = $data['match'];
+		$r['games'] = $data['games'];
+
+		$r['state'] = [];
+		if ($data['games']) {
+			$lastGame = end($data['games']);
+			foreach ([1, 2] as $side) {
+				$r['state']['side' . $side]['games'] = $data['match']['side' . $side . '_games'];
+				$r['state']['side' . $side]['points'] = $lastGame['side' . $side . '_points'];
+			}
+
+			foreach ($data['players'] as $player) {
+				$r['state']['side' . $player['side']]['player'] = $player['first_name'];
+			}
+		}
 
 		return $r;
+	}
+
+	private function xhr_scorePlus($request) {
+		$match_id = (int) $request['match'];
+		$side = (int) $request['side'];
+
+		$game = DB::val("SELECT game FROM games WHERE won_by_side IS NULL AND match_id = " . $match_id);
+		DB::q("INSERT INTO points (match_id, game, side) VALUES (" . $match_id . ", " . $game . ", " . $side . ")");
+
+		$this->recalculateMatch($match_id);
+		return $this->xhr_updateMatch(['match' => $match_id]);
+	}
+
+	private function xhr_scoreUndo($request) {
+		$match_id = (int) $request['match'];
+		DB::q("DELETE FROM points WHERE match_id = " . $match_id . " ORDER BY id DESC LIMIT 1");
+		$data = self::getMatchData($match_id);
+		$lastGame = end($data['games']);
+		if ($lastGame['side1_points'] == 0 && $lastGame['side2_points'] == 0 && count($data['games']) > 1) {
+			DB::q("DELETE FROM games WHERE match_id = " . $match_id . " AND game = " . $lastGame['game']);
+			DB::q("UPDATE games SET won_by_side = NULL WHERE match_id = " . $match_id . " AND game = " . ($lastGame['game'] - 1));
+		}
+
+		$this->recalculateMatch($match_id);
+		return $this->xhr_updateMatch(['match' => 1]);
+	}
+
+	private function recalculateMatch($match_id) {
+		$data = self::getMatchData($match_id);
+		$lastGame = end($data['games']);
+		$side1points = $lastGame['side1_points'];
+		$side2points = $lastGame['side2_points'];
+		$mostPoints = max($side1points, $side2points);
+		$pointsDiff = abs($side1points - $side2points);
+
+		// game won
+		if ($mostPoints >= 11 && $pointsDiff >= 2) {
+			$wonBySide = $side1points > $side2points ? 1 : 2;
+			DB::q("UPDATE games SET won_by_side = " . $wonBySide . " WHERE match_id = " . $match_id . " AND game = " . $lastGame['game']);
+
+			// match won
+			if ($data['match']['side' . $wonBySide . '_games'] + 1 > $data['match']['best_out_of'] / 2) {
+				DB::q("UPDATE matches SET won_by_side = " . $wonBySide . ", finished_at = NOW() WHERE id = " . $match_id);
+			// new game
+			} else {
+				DB::q("INSERT INTO games (match_id, game) VALUES (" . $match_id . ", " . (count($data['games']) + 1) . ")");
+			}
+		}
 	}
 }
