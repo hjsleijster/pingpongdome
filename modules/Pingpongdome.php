1365439ae8057a034686c7891568cfe9f73b67ab
Henk-Jan Sleijster <henk-jan@sleijster.nl>
2025-03-30 23:09:38 +0200

    fix incorrect game count with subquery

--- modules/Pingpongdome.php
+++ modules/Pingpongdome.php
@@ -109,9 +109,17 @@ class Pingpongdome
 
 		$data = [
 			'match' => DB::row("
-				SELECT m.*, IFNULL(SUM(g.won_by_side = 1), 0) `side1_games`, IFNULL(SUM(g.won_by_side = 2), 0) `side2_games`
+				SELECT m.*, g.side1_games, g.side2_games
 				FROM matches m
-				LEFT JOIN games g ON m.id = g.match_id AND g.won_by_side IS NOT NULL
+				LEFT JOIN (
+					SELECT
+						match_id
+						, SUM(won_by_side = 1) AS side1_games
+						, SUM(won_by_side = 2) AS side2_games
+					FROM games
+					WHERE won_by_side IS NOT NULL
+					GROUP BY match_id
+				) AS g ON m.id = g.match_id
 				WHERE deleted_at IS NULL AND m.id = " . $match_id . "
 				GROUP BY m.id"
 			),
